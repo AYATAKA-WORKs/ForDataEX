eaf340c436304e672a2bce9c9df3dca4bc82d735
AyaTaka <32975434+AYATAKA-WORKs@users.noreply.github.com>
2024-09-17 12:01:10 +0900

    Add files via upload

---
+++ Pre001_Initialize.m
@@ -0,0 +1,14 @@
+clear all
+close all
+
+% Physical property
+R = 287;    % [J/kg/K]
+kap = 1.4;  % [-]
+
+% Initial Condition
+T_init = 293.15;    % [K]
+P_init = 101325;    % [Pa]
+
+% Boundary Condition
+T_amb = 293.15;    % [K]
+P_amb = 101325;    % [Pa]
